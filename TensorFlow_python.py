b02983b270ce8fbed310974fc4d40f096b225bad
Timz Owen <ktimothy@kabarak.ac.ke>
2019-12-10 21:36:22 +0300

    Update TensorFlow_python.py
    added some essential py expressions

--- TensorFlow_python.py
+++ TensorFlow_python.py
@@ -12,3 +12,16 @@ for i in range(8,25,5): #i=8, 13, 18,23 (start stop stop)
     print("--- now running with i: {}".format(i))
     r = HelloWorldXY(i, i)
     print("result from hello world: {} ".format(i, r))
+    
+    ##doing exponential functions 
+    print(3**2)
+    #9
+    
+    #modulo operator which returns the remainder of division 
+    print(9%2)
+    #1
+    
+    #Integer division which rounds off result to the next integer
+    print(7%2)
+    #3
+    
