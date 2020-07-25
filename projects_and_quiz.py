6080654192016bfbcb757da9ffb09c934c3243fb
Timz Owen <ktimothy@kabarak.ac.ke>
2020-07-25 19:24:34 +0300

    Update projects_and_quiz.py
    while loops

--- projects_and_quiz.py
+++ projects_and_quiz.py
@@ -435,6 +435,7 @@ while x<10:
     x+=1
 print(sum)
 
+
 # 31
 #Fix the problrm caused by invalid answers below
 def count_down(start_number):
@@ -443,4 +444,75 @@ def count_down(start_number):
     current -= 1
   print("Zero!")
 count_down(3)
+
+
+# 32
+#figure out the problem in the infinite loop and correct
+def print_range(start, end):
+    	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+            print(n)
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+
+#Anws 
+def print_range(start, end):
+    	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+            print(n)
+            n+=1
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
+
+#33 
+#write code to print_prime_factors function print all 
+#the prime factors of a number. A prime factor is
+#a number that is prime and divides another without a remainder.
+def print_prime_factors(number):
+      # Start with two, which is the first prime
+  factor = 2
+  # Keep going until the factor is larger than the number
+  while factor <= number:
+    # Check if factor is a divisor of number
+    if number % factor == 0:
+      # If it is, print it and divide the original number
+      print(factor)
+      number = number / factor
+    else:
+      # If it's not, increment the factor by one
+      factor += 1
+  return "Done"
+
+print_prime_factors(100)
+# Should print 2,2,5,5
+
+# 34:
+#The following code can lead to an infinite loop.
+#  Fix the code so that it can finish successfully for all numbers.
+def is_power_of_two(n):
+      # Check if the number can be divided by two without a remainder
+  while n % 2 == 0:
+    n = n / 2
+  # If after dividing by two the number is 1, it's a power of two
+  if n/2 == 1:
+    return True
+  return False
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+#Ans
+def is_power_of_two(n):
+      # Check if the number can be divided by two without a remainder
+  while n % 2 == 0 and n!= 0:
+    n = n / 2
+  # If after dividing by two the number is 1, it's a power of two
+  if n/2 == 1:
+    return True
+  return False
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
 
