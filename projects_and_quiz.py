768d36c75d0e863ad2e1727b449a9e2b5d71351e
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-01 10:43:32 +0300

    Update projects_and_quiz.py
    recursion more quizes and solutions

--- projects_and_quiz.py
+++ projects_and_quiz.py
@@ -768,3 +768,111 @@ retry(stop_service, 5)
 # 51
 # Using recursion create a function that calls itself using a factorial sample
 
+# Using recursion create a function that calls itself using a factorial sample
+def factorial(n):
+    print("Factorial called with: " + str(n))
+    if n < 2:
+        return 1
+    result = n * factorial(n-1)
+    print("Returning: " + str(result) + " For factorial " + str(n))
+    return result
+factorial(5)
+
+# 52
+# write a function sum_positive_numbers should return the sum of all positive numbers
+# between the number n received and 1.
+# For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should
+# return 1+2+3+4+5=15. Fill in the gaps to make this work:
+def sum_positive_numbers(n):
+    # The base case is n being smaller than 1
+    print("The positive sum is for number : " + str(n))
+    if n < 1:
+        return 0
+    total = n + sum_positive_numbers(n-1)
+    return total
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be
+
+# 53
+# Fill in the blanks to make the is_power_of function return whether the number is a power of the given base. 
+# Note: base is assumed to be a positive number. Tip: for functions that return a boolean value, you can return the result of a comparison.
+def is_power_of(number, base):
+      # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return __
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(__, ___)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
+
+#ans
+def is_power_of(number, base):
+      # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return number == 1
+  result = number//base
+  # Recursive case: keep dividing number by base.
+  return is_power_of(result, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
+
+# 54
+# The count_users function recursively counts the amount of users that belong to a group 
+# in the company system, by going through each of the members of a group 
+# and if one of them is a group, recursively calling the function and counting the members. 
+# But it has a bug! Can you spot the problem and fix it?
+
+def count_users(group):
+      count = 0
+      for member in get_members(group):
+             count += 1
+             if is_group(member):
+                   count += count_users(member)
+                   return count
+
+print(count_users("sales")) # Should be 3
+print(count_users("engineering")) # Should be 8
+print(count_users("everyone")) # Should be 18
+
+#ans
+def count_users(group):
+      count = 0
+   for member in get_members(group):
+    #count += 1
+    if is_group(member):
+      count += count_users(member)
+    else:
+      count += 1
+  return count
+
+print(count_users("sales")) # Should be 3
+print(count_users("engineering")) # Should be 8
+print(count_users("everyone")) # Should be 18
+
+# 55
+#Implement the sum_positive_numbers function,
+#  as a recursive function that returns the sum of all positive numbers
+#  between the number n received and 1. For 
+# example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
+
+def sum_positive_numbers(n):
+      return 0
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+
+#ans
+def sum_positive_numbers(n):
+      if n < 1:
+       return 0
+      total_sum = n + sum_positive_numbers(n-1)
+       return total_sum
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
