4279fa8f6248cc5cb5591fb1689813bbb3418c0b
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-17 00:06:05 +0300

    Update quizes_projects_4_OOP.py
    Hello, classes .py

--- quizes_projects_4_OOP.py
+++ quizes_projects_4_OOP.py
@@ -1,1 +1,19 @@
+# OBJECT ORIENTED PROGRAMMING WITH PYTHON QUIZES AND PROJECTS:
 
+# Declaring a class: (empty class uses pass to show it has no members or attributes)
+class Fruits:
+    pass
+
+#try out this
+class Apple:
+    color = ""
+    size = ""
+    taste = ""
+# assign a varibale the function to make it callable
+callAple = Apple()
+# now assign values to them using the .DOT notation
+callAple.color = "Green"
+callAple.size = "Small"
+callAple.taste = "very sweet"
+# print the elements
+print(callAple.color, callAple.size,callAple.taste, end="")
