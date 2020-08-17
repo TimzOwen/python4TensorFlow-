3a1d2719fae73fde1a9d7a179bccc9a0d9f31645
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-17 10:51:09 +0300

    Update quizes_projects_4_OOP.py
    soln to OOP quizzes update

--- quizes_projects_4_OOP.py
+++ quizes_projects_4_OOP.py
@@ -226,3 +226,120 @@ def max_elevation_city(min_population):
 print(max_elevation_city(100000)) # Should print "Cusco, Peru"
 print(max_elevation_city(1000000)) # Should print "Sofia, Bulgaria"
 print(max_elevation_city(10000000)) # Should print ""
+
+
+
+#soln
+# define a basic city class
+class City:
+    name = ""
+    country = ""
+    elevation = 0
+    population = 0
+
+
+# create a new instance of the City class and
+# define each attribute
+city1 = City()
+city1.name = "Cusco"
+city1.country = "Peru"
+city1.elevation = 3399
+city1.population = 358052
+
+# create a new instance of the City class and
+# define each attribute
+city2 = City()
+city2.name = "Sofia"
+city2.country = "Bulgaria"
+city2.elevation = 2290
+city2.population = 1241675
+
+# create a new instance of the City class and
+# define each attribute
+city3 = City()
+city3.name = "Seoul"
+city3.country = "South Korea"
+city3.elevation = 38
+city3.population = 9733509
+
+
+def max_elevation_city(min_population):
+    # Initialize the variable that will hold
+    # the information of the city with
+    # the highest elevation
+    return_city = City()
+
+    # Evaluate the 1st instance to meet the requirements:
+    # does city #1 have at least min_population and
+    # is its elevation the highest evaluated so far?
+    if city1.population >= min_population and city1.elevation > return_city.elevation:
+        return_city = city1
+    # Evaluate the 2nd instance to meet the requirements:
+    # does city #2 have at least min_population and
+    # is its elevation the highest evaluated so far?
+    if city2.population >= min_population and city2.elevation > return_city.elevation:
+        return_city = city2
+    # Evaluate the 3rd instance to meet the requirements:
+    # does city #3 have at least min_population and
+    # is its elevation the highest evaluated so far?
+    if city3.population >= min_population and city3.elevation > return_city.elevation:
+        return_city = city3
+
+    # Format the return string
+    if return_city.name:
+        return ("{}, {}".format(return_city.name, return_city.country))
+    else:
+        return ""
+
+
+print(max_elevation_city(100000))  # Should print "Cusco, Peru"
+print(max_elevation_city(1000000))  # Should print "Sofia, Bulgaria"
+print(max_elevation_city(10000000))  # Should print ""
+
+
+# 4
+# We have two pieces of furniture: a brown wood table and a red leather couch. Fill in the blanks following
+#  the creation of each Furniture class instance, so that the describe_furniture function can format a 
+# sentence that describes these pieces as follows: "This piece of furniture is made of {color} {material}"
+
+class Furniture:
+    	color = ""
+	material = ""
+
+table = Furniture()
+___
+___
+
+couch = Furniture()
+___
+___
+
+def describe_furniture(piece):
+	return ("This piece of furniture is made of {} {}".format(piece.color, piece.material))
+
+print(describe_furniture(table)) 
+# Should be "This piece of furniture is made of brown wood"
+print(describe_furniture(couch)) 
+# Should be "This piece of furniture is made of red leather"
+
+
+#soln
+class Furniture:
+    color = ""
+	material = ""
+
+table = Furniture()
+table.color="brown"
+table.material="wood"
+
+couch = Furniture()
+couch.color="red"
+couch.material="leather"
+
+def describe_furniture(piece):
+	return ("This piece of furniture is made of {} {}".format(piece.color, piece.material))
+
+print(describe_furniture(table))
+# Should be "This piece of furniture is made of brown wood"
+print(describe_furniture(couch))
+# Should be "This piece of furniture is made of red leather"
