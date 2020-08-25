2cfb1c34919e8eec11d2e1a29817f36e94aa3e41
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-25 21:42:52 +0300

    Update Z_Final_project.py
    Problem statement

--- Z_Final_project.py
+++ Z_Final_project.py
@@ -1,1 +1,5 @@
 # working on final project to combine all the learnt concepts into 1
+# problem statement.
+#The CTO wants to monitor all the computer usage by all engineers. Using Python ,
+# write an automation script that will produce a report when each user logged in and out,
+# and how long each user used the computers.
