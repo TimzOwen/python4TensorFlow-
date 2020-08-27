da1bbc09d4525ba92788b16cf225374d01cd11e1
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-27 05:39:25 +0300

    Create project_final_project_word_cloud.py
    problem statement

---
+++ project_final_project_word_cloud.py
@@ -0,0 +1,4 @@
+
+#FINAL PROJECT OF SYSTEM AUTOMATION:
+# using word cloud
+# Create a dictionary with words and word frequencies that can be passed to the generate_from_frequencies function of the WordCloud class.
