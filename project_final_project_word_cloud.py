e6bd64c54a218a52f232ca9bc5cc7b7ea1a1ba57
Timz Owen <ktimothy@kabarak.ac.ke>
2020-08-27 05:40:45 +0300

    Update project_final_project_word_cloud.py
    guidelines and suggestions.

--- project_final_project_word_cloud.py
+++ project_final_project_word_cloud.py
@@ -2,3 +2,64 @@
 #FINAL PROJECT OF SYSTEM AUTOMATION:
 # using word cloud
 # Create a dictionary with words and word frequencies that can be passed to the generate_from_frequencies function of the WordCloud class.
+
+#snipset of generating images with word cloud
+cloud = wordcloud.WordCloud()
+cloud.generate_from_frequencies(frequencies)
+cloud.to_file("myfile.jpg")
+
+# start writing code:
+
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# load the file with text
+# This is the uploader widget
+
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+# remove unnecessary wordings
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+
+    # write code to complete the task
+
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies()
+    return cloud.to_array()
+    
