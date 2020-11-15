b25d589c29853588bc568633837da9330d186f46
Timz Owen <ktimothy@kabarak.ac.ke>
2020-11-15 19:58:41 +0300

    Update Hello_worldTensorflow.py
    using with

--- Hello_worldTensorflow.py
+++ Hello_worldTensorflow.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 import tensorflow as tf
 
 #Create op ,
-#start session 
+#start session
+# you can use the with()_to help start and close the app directly 
 #print the op in the String format 
 hello = tf.constant('Hello World, TensorFlow! Here')
 
